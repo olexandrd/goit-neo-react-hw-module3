90edff703cc025b42c91cf741bcc4e99774d397c
Olexandrd <olexandrd@gmail.com>
2024-08-08 19:31:05 +0300

    Use localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
 
-const savedContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2e", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-2er", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-22", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  { id: "id-5", name: "Annie Copeland", number: "227-91-26" },
-];
-
 const App = () => {
   const [searchRequest, setSearchRequest] = useState("");
-  const [contacts, setContacts] = useState(savedContacts);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem("savedContacts");
+    if (savedContacts !== null) {
+      return JSON.parse(savedContacts);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("savedContacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(searchRequest.toLowerCase())
